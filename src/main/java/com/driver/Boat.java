d6ed5d21bb5eeb9c0af9a6e63a9fdd1427b21cf1
Asif <muhammadasifazmi@gmail.com>
2023-04-15 13:49:13 +0530

    Third submit

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -12,4 +12,12 @@ public class Boat implements WaterVehicle{
     public int getVehicleCapacity() {
         return capacity;
     }
+
+    public Boat() {
+    }
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
 }
